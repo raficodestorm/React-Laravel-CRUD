3638da08212392a56ef2500c1d61c261d3e0d395
S-A-Rafi <sarafi3258@gmail.com>
2025-12-03 13:09:27 +0600

    up

---
+++ laravel-dektasi/app/Http/Controllers/Api/StudentController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class StudentController extends Controller
+{
+    public function index(){
+        $students = Student::all();
+        if($students->count() > 0) {
+            return response()->json([
+                'status' => 200,
+                'students' => $students
+            ], 200);
+        }
+        else {
+            return response()->json([
+                'status' => 404,
+                'students' => 'No records found'
+            ], 404); 
+        }
+    }
+
+    public function store(Request $request){
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|string|max:191',
+            'course' => 'required|string|max:191',
+            'email' => 'required|email|max:191',
+            'phone' => 'required|string|max:20',
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->messages()
+            ],422);
+        }
+        else{
+            $student = Student::create([
+                'name' => $request->name,
+                'course' => $request->course,
+                'email' => $request->email,
+                'phone' => $request->phone,
+            ]);
+            if($student){
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Student created successfully',
+                ], 200);
+            }
+            else{
+                return response()->json([
+                    'status' => 500,
+                    'message' => 'Something went wrong',
+                ], 500);
+            }
+        }
+    }
+}
---
+++ laravel-dektasi/routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\StudentController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('students', [StudentController::class, 'index']);
+Route::post('students', [StudentController::class, 'store']);
